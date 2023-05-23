a25de0baf8976369f1efc4212a0786116e77f73a
Cameron Yuen <cameronyuen@Cams-MBP.verizon.net>
2023-05-23 00:46:40 -0400

    wip

--- alt-meal-program-client/src/config.js
+++ alt-meal-program-client/src/config.js
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-export const axiosInstance = axios.create({
+export const axiosInstance = Axios.create({
     baseURL = "https://alternativemealprogram.herokuapp.com"
 });
 
